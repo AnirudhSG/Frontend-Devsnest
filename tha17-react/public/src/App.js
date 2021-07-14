62dd4735b0aa0ca5559f3ab0aea1358e744a3d36
AnirudhSG <asgangwar17@gmail.com>
2021-07-14 20:12:19 +0530

    tha added

--- tha17-react/public/src/App.js
+++ tha17-react/public/src/App.js
@@ -4,10 +4,16 @@ import Card from './Card'
 function App() {
   return (
     <>
+    <h1>Calorie Read Only</h1>
+    <div className="cont">
       <Card title="paneer" calorie="200"/>
       <Card title="egg" calorie="50"/>
       <Card title="chicken" calorie="300"/>
       <Card title="banana" calorie="100"/>
+      <Card title="banana" calorie="100"/>
+      <Card title="banana" calorie="100"/>
+      <Card title="banana" calorie="100"/>
+      </div>
   </>
   );
 }
--- tha17-react/public/src/Card.js
+++ tha17-react/public/src/Card.js
@@ -2,9 +2,10 @@ import React from "react";
 function Card(props) {
     return(
         <>
-    <div className="container"></div>
+    <div className="container">
     <div className="title">{props.title}</div>
     <div className="calories">you have consumed {props.calorie}cals today</div>
+    </div>
     </>
     );
 };
