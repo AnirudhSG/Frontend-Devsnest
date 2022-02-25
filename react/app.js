29510d3977fe4175916e5b4ec7904dadcf9ecc84
Anirudh <asgangwar17.com>
2022-02-25 10:38:09 +0530

    commit 24 feb

---
+++ react/app.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from './meme.jpg';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="meme" style={{height: '500px', width:'500px' ,border:'5px solid green', borderRadius: '20px'}}/>
+        
+  
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
